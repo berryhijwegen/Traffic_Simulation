4bf88481c2ad073b7d5cfb3f7cc7b1b8dfe6c003
Berry <berry.hijwegen@student.hu.nl>
2019-12-06 20:10:32 +0100

    First commit

---
+++ unity_project/Assets/Patrol2.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Patrol2 : MonoBehaviour
+{
+    public Transform[] points;
+    private int destPoint = 0;
+    public float step;
+    public transform destination;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GotoNextPoint();
+    }
+
+    void GotoNextPoint()
+    {
+        // Returns if no points have been set up
+        if (points.Length == 0)
+            return;
+
+        // Set the agent to go to the currently selected destination.
+        destination = points[destPoint].position;
+
+        // Choose the next point in the array as the destination,
+        // cycling to the start if necessary.
+        destPoint = (destPoint + 1) % points.Length;
+    }
+
+
+    void Update()
+    {
+        // Choose the next destination point when the agent gets
+        // close to the current one.
+        if (Vector3.Distance(transform.position, destination.position) < 0.5f) {
+            GotoNextPoint();
+        }
+        step =  speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+    }
+}
